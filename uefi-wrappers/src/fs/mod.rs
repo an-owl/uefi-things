92041d2fe2e7ad98f5b049b067b5d547a7857d22
an_owl <an_owl@protonmail.com>
2021-10-13 14:17:49 +1300

    I would like to be able to write to these.

--- uefi-wrappers/src/fs/mod.rs
+++ uefi-wrappers/src/fs/mod.rs
@@ -4,21 +4,21 @@ use alloc::string::{String, ToString};
     /// This function returns a FileHandle from a given path relative to root.
     /// Returns error message on Err.
     /// Does not discriminate on trailing slashes
-pub fn get_file_from_path(fs: &mut SimpleFileSystem, path: &str) -> Result<FileHandle,String> {
+pub fn get_file_from_path(fs: &mut SimpleFileSystem, path: &str, mode: FileMode, attributes: FileAttribute) -> Result<FileHandle,String> {
     let mut root = fs.open_volume()
         .expect("Failed to open filesystem root").log();
     if ! path.starts_with('/'){
         return Result::Err("Invalid path".to_string());
     };
 
-    let mut current_file = root.open(".",FileMode::Read,FileAttribute::READ_ONLY)
+    let mut current_file = root.open(".",mode,attributes)
         .expect("Failed to get root handle. please open issue on github").log();
 
     let path_it = path.split('/');
 
     for file in path_it{
         //there is probably a better way to do this
-        let new_file_result = current_file.open(file,FileMode::Read,FileAttribute::READ_ONLY);
+        let new_file_result = current_file.open(file,mode,attributes);
         let new_file;
 
         match new_file_result{
