3ae6c166741795f0e08134aa34021a7bf96c791f
an_owl <an_owl@protonmail.com>
2021-11-23 17:51:36 +1300

    changed get_proto ro return uefi::Result

--- uefi-wrappers/src/proto.rs
+++ uefi-wrappers/src/proto.rs
@@ -1,23 +1,25 @@
 use uefi::proto::Protocol;
 use uefi::prelude::BootServices;
-use uefi::Status;
+use uefi::{Status, Completion};
 
 
 
 /// fetches and returns given protocol
 /// Err contains the uefi::status returned
 /// Ok returns &mut protocol
-pub fn get_proto<T: Protocol>(bs: &BootServices) -> Result<&'static mut T,Status>
+pub fn get_proto<T: Protocol>(bs: &BootServices) -> uefi::Result<&'static mut T>
 {
     let protocol;
     return match bs.locate_protocol::<T>() {
         Ok(proto) => {
             let proto = proto.log();
             protocol = unsafe { &mut *proto.get() };
-            Ok(protocol)
+
+            //TODO return the actual status instead of just Success
+            Ok(Completion::new(Status::SUCCESS,protocol))
         }
         Err(i) => {
-            Err(i.status())
+            Err(i)
         }
     };
 }
