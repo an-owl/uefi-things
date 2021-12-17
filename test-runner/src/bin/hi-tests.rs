24d6ae8b6cd59ed60759582f098c6cf39d56bfba
an_owl <an_owl@protonmail.com>
2021-12-17 17:53:49 +1300

    added tests for draw_to_buff()

--- test-runner/src/bin/hi-tests.rs
+++ test-runner/src/bin/hi-tests.rs
@@ -2,6 +2,7 @@
 #![no_std]
 #![feature(abi_efiapi)]
 
+//! hi not as in hello but as in human interface
 
 extern crate rlibc;
 extern crate alloc;
@@ -18,6 +19,7 @@ fn main(image: Handle, mut st: SystemTable<Boot>) -> Status {
     let mut tests = Vec::new();
 
     tests.push(test_runner::Test::new("test graphics draw", tests::test_graphics_things));
+    tests.push(test_runner::Test::new("test drw_to_buff", tests::test_receive_sprite));
 
     test_runner::test_runner(tests, image, &st);
     Status::SUCCESS
@@ -42,7 +44,7 @@ pub mod tests {
     use uefi::proto::media::file::FileType;
     use uefi::proto::console::text::Output;
     use alloc::string::ToString;
-
+    const SECOND: usize = 1000000;
     pub fn test_graphics_things(_table: Handle, st: &SystemTable<Boot>) -> TestResult{
         const WIDTH: usize = 800;
         const HEIGHT: usize = 600;
@@ -94,5 +96,72 @@ pub mod tests {
         st.boot_services().stall(2000000);
 
         Unknown(Status::SUCCESS,"Requires Human Verification")
+    }
+
+    pub fn test_receive_sprite(_table: Handle, st: &SystemTable<Boot>) -> TestResult{
+        let gop = get_proto::<GraphicsOutput>(st.boot_services()).unwrap().unwrap();
+        let fs = get_proto::<SimpleFileSystem>(st.boot_services()).unwrap().unwrap();
+        let o = get_proto::<Output>(st.boot_services()).unwrap().unwrap();
+
+        let mut g = GraphicsHandle::new(gop,None);
+
+        let bg_file =  match uefi_wrappers::fs::get_file_from_path(fs,
+                                                    "/test-img.ppm",
+                                                    uefi::proto::media::file::FileMode::Read,
+                                                    uefi::proto::media::file::FileAttribute::empty())
+            .into_type().unwrap(){
+            FileType::Regular(f) => {
+                read_file(f)
+            }
+            FileType::Dir(_) => {
+
+                return Fail(Status::LOAD_ERROR,"Found Directory")
+            }
+        };
+
+        let mut bg = Sprite::new(g.get_resolution().0,g.get_resolution().1);
+        let mut fg = Sprite::new(171,226);
+        bg.read_ppm(&bg_file).unwrap();
+        drop(bg_file);
+
+        let fg_file =  match uefi_wrappers::fs::get_file_from_path(fs,
+                                                    "/smolspite.ppm",
+                                                    uefi::proto::media::file::FileMode::Read,
+                                                    uefi::proto::media::file::FileAttribute::empty())
+            .into_type().unwrap(){
+            FileType::Regular(f) => {
+                read_file(f)
+            }
+            FileType::Dir(_) => {
+
+                return Fail(Status::LOAD_ERROR,"Found Directory")
+            }
+        };
+
+        fg.read_ppm(&fg_file).unwrap();
+        drop(fg_file);
+
+
+
+        g.insert_buff(bg);
+        g.draw(0);
+        st.boot_services().stall(1*SECOND);
+
+        g.draw_to_buff(&fg,0,(0,0));
+        g.draw(0);
+        st.boot_services().stall(1*SECOND);
+
+        let (mut width,mut height) = g.get_resolution(); //sets cords to place in bottom right corner
+        width  -= fg.resolution().0;
+        height -= fg.resolution().1;
+        writeln!(o,"x: {}",width).unwrap();
+        writeln!(o,"y: {}",height).unwrap();
+        st.boot_services().stall(3*SECOND);
+        g.draw_to_buff(&fg,0,(width,height));
+
+        g.draw(0);
+        st.boot_services().stall(3*SECOND);
+
+        Unknown(Status::SUCCESS,"Requires human verification")
     }
 }
